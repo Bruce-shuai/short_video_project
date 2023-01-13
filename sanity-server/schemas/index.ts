5793414dbc7654f9ba942fea5258623cbbaaf286
heyang <284970670@qq.com>
2023-01-13 12:46:27 +0800

    feat: Sanity Schemas finish

--- sanity-server/schemas/index.ts
+++ sanity-server/schemas/index.ts
@@ -1,1 +1,27 @@
-export const schemaTypes = []
+import post from './post';
+import comment from './comment';
+import user from './user';
+import postedBy from './postedBy';
+
+/**
+ * 几者关系
+ * 
+ *      user <---一对一---> post    (通过postedBy建立关系)
+ *      user <---一对一---> like    (直接 to User建立关系)
+ *      user <---一对一---> comment (通过postedBy建立关系)
+ *      post <---一对多---> comment
+ *      post <---一对多---> likes
+ * 
+ *                   |--> comment
+ *     user -> post -|--> likes
+ * 
+ * TODO: 为什么需要有 postedBy 而不直接 to User？
+ * 因为在Sanity中为reference提供了专门的组件
+ */
+
+export const schemaTypes = [
+  post,
+  comment,
+  user,
+  postedBy
+]
